7d2210f1ea40040aecc61b8e91383af82e228866
GiuliaAllegri21 <92572841+GiuliaAllegri21@users.noreply.github.com>
2021-10-26 12:09:59 +0200

    Create Create R_code_remote_sensing.r

---
+++ Create R_code_remote_sensing.r
@@ -0,0 +1,15 @@
+# R code for ecosystem monitoring by remote sensing
+# First of all we need to install addictional packages 
+# https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/install.packages 
+# https://cran.r-project.org/web/packages/raster/raster.pdf
+
+install.packages("terra")
+
+# we need to use quotes for things there are not in R
+install.packages("raster")
+
+# how to use library in r 
+# https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/library
+# with the function library we can use packages
+
+library(raster)
